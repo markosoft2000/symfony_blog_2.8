7dc4651a484932f819602932b837247db1a77de9
Marko <markosoft2000@yandex.ru>
2018-03-25 02:21:24 +0200

    part 2 forms and mailing

---
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Blogger\BlogBundle\Entity;
+
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
+class Enquiry
+{
+    protected $name;
+
+    protected $email;
+
+    protected $subject;
+
+    protected $body;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @param ClassMetadata $metadata
+     */
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+        $metadata->addPropertyConstraint('email', new Email([
+            'message' => 'Symblog does not like invalid emails. Give me a real one!'
+        ]));
+        $metadata->addPropertyConstraint('subject',  new Length([
+            'max' => 50
+        ]));
+        $metadata->addPropertyConstraint('body', new Length([
+            'min' => 50
+        ]));
+    }
+}
