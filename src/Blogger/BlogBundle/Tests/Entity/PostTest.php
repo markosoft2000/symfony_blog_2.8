5deb9dd812656e0f43281cfacfd721a2002cd7d2
Marko <markosoft2000@yandex.ru>
2018-04-08 14:56:52 +0200

    some extra tests

--- src/Blogger/BlogBundle/Tests/Entity/PostTest.php
+++ src/Blogger/BlogBundle/Tests/Entity/PostTest.php
@@ -6,6 +6,56 @@ use Blogger\BlogBundle\Entity\Post;
 
 class PostTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Post
+     */
+    private $post;
+
+    public function setUp()
+    {
+        $this->post = new Post();
+
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        unset($this->post);
+
+        parent::tearDown();
+    }
+
+    public function providerTestSlug()
+    {
+        return [
+            array('Hello World', 'hello-world'),
+            array('A Day With Symfony2', 'a-day-with-symfony2'),
+            array('Hello world', 'hello-world'),
+            array('symblog ', 'symblog'),
+            array(' symblog', 'symblog'),
+        ];
+    }
+
+    /**
+     * @dataProvider providerTestSlug
+     * @param string $originalString
+     * @param string $expectedResult
+     */
+    public function testSlugifyWithDataProvider($originalString, $expectedResult)
+    {
+        $this->assertEquals($expectedResult, $this->post->slugify($originalString));
+    }
+
+    public function testPrivateWithReflection()
+    {
+        $reflection = new \ReflectionClass(get_class($this->post));
+        $method = $reflection->getMethod('slugify');
+        $method->setAccessible(true);
+
+        $result = $method->invokeArgs($this->post, ['A Day With Symfony2']);
+        $this->assertEquals('a-day-with-symfony2', $result);
+    }
+
     public function testSlugify()
     {
         $post = new Post();
